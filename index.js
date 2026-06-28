60a832b50c26bb3afe07e8a4a9c2fd5b96c6ef93
eliasvestman-dotcom <eliasvestman@gmail.com>
2026-06-28 20:00:14 +0000

    Update to latest mineflayer 4.37.1 and @nbsjs/core 6.0.0

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
-const fs = require("fs");
-const { fromArrayBuffer } = require("@encode42/nbs.js"); // Import NBS.js
-const chalk = require('chalk');
-var mineflayer = require("mineflayer");
-var parseSentence = require("minimist-string");
-var config = require("./config.json");
-var nb = require("./multi.js");
+import fs from "fs";
+import { read as readNBS } from "@nbsjs/core";
+import chalk from "chalk";
+import mineflayer from "mineflayer";
+import parseSentence from "minimist-string";
+import { createRequire } from "module";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { NoteBot } from "./multi.js";
+
+const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
 
 const numWorkers = 4;
-module.exports.currentSong = null;
+let currentSong = null;
 
-var options = {
-    username: config.bot.username || "notebot",
-    host: config.bot.host || "localhost",
-    port: config.bot.port || 25565,
-    version: config.bot.version || "1.20.1",
-}
+const options = {
+  username: config.bot.username || "notebot",
+  host: config.bot.host || "localhost",
+  port: config.bot.port || 25565,
+  version: config.bot.version || "1.20.1",
+};
 
-var bot = mineflayer.createBot(options);
-var workers = [];
+const bot = mineflayer.createBot(options);
+const workers = [];
 
 bot.on("login", () => {
-  bot.chat(`Mineflayer Notebot by @meeplabsdev on github`);
+  bot.chat(`Mineflayer Notebot by @meeplabsdev on github (Updated v2.0)`);
 
-  for (i = 1; i <= numWorkers; i++) {
-    let username = `notebot_worker${twoNum(i)}`;
-    workers.push(new nb.NoteBot(username));
+  for (let i = 1; i <= numWorkers; i++) {
+    const username = `notebot_worker${twoNum(i)}`;
+    workers.push(new NoteBot(username));
   }
 });
 
 bot.on("kicked", (reason) => {
-  respond(`I got kicked for ${reason}`, 2)
-  respond(`Rage Quitting`, 2)
+  respond(`I got kicked for ${reason}`, 2);
+  respond(`Rage Quitting`, 2);
   process.exit();
-})
+});
 
 bot.on("chat", (username, message) => {
-  if (username == options.username) {
-    respond(message)
+  if (username === options.username) {
+    respond(message);
   }
 });
 
-bot.on("whisper", (username, message, rawMessage) => {
+bot.on("whisper", (username, message) => {
   if (config.commands_perms.includes(username)) {
     handle(message, username);
   }
@@ -50,16 +58,16 @@ function respond(message, level = 0) {
   let mes = message;
   switch (level) {
     case -1:
-      mes = chalk.green("[DEBUG] " + message); // debug
+      mes = chalk.green("[DEBUG] " + message);
       break;
     case 0:
-      mes = chalk.blue("[INFO]  " + message); // info
+      mes = chalk.blue("[INFO]  " + message);
       break;
     case 1:
-      mes = chalk.yellow("[WARN]  " + message); // warning
+      mes = chalk.yellow("[WARN]  " + message);
       break;
     case 2:
-      mes = chalk.red("[ERROR] " + message); // error
+      mes = chalk.red("[ERROR] " + message);
       break;
   }
   console.log(mes);
@@ -69,30 +77,39 @@ function respond(message, level = 0) {
 }
 
 function beep() {
-  require("child_process").exec("powershell.exe [console]::beep(500,600)");
+  // Platform-specific beep implementation
+  if (process.platform === "win32") {
+    require("child_process").exec('powershell.exe [console]::beep(500,600)');
+  } else if (process.platform === "darwin") {
+    require("child_process").exec("printf '\\\\a'");
+  } else {
+    require("child_process").exec("echo -n $'\\\\a'");
+  }
 }
 
 function handle(command, username) {
-  if(!command.startsWith(config.settings.command_prefix + options.username)) return false;
-  var cmd = command.substring(1);
-  cmd = parseSentence(cmd);
-  delete cmd["_"]
+  if (!command.startsWith(config.settings.command_prefix + options.username))
+    return false;
+  const cmd = parseSentence(command.substring(1));
+  delete cmd["_"];
 
-  switch(Object.keys(cmd)[0]) {
+  const action = Object.keys(cmd)[0];
+
+  switch (action) {
     case "detect":
-      workers.forEach(async worker => {
-        setTimeout(() => { worker.detect(); }, 100);
-      })
+      workers.forEach((worker) => {
+        setTimeout(() => {
+          worker.detect();
+        }, 100);
+      });
       break;
     case "play":
       respond(`Playing ${cmd.play}`);
       if (!isValidFile(cmd.play)) {
         respond(`${cmd.play} is not a valid file!`, 1);
       } else {
-        let songFile = fs.readFileSync("songs/" + cmd.play); // Read a NBS file
-        let buffer = new Uint8Array(songFile).buffer; // Create an ArrayBuffer
-        let song = fromArrayBuffer(buffer); // Parse song from ArrayBuffer
-
+        const songFile = fs.readFileSync("songs/" + cmd.play);
+        const song = readNBS(new Uint8Array(songFile));
         play(song, cmd.speed || 100);
       }
       break;
@@ -100,13 +117,13 @@ function handle(command, username) {
       if (!isValidFile(cmd.setup)) {
         respond(`${cmd.setup} is not a valid file!`, 1);
       } else {
-        let songFileReq = fs.readFileSync("songs/" + cmd.setup); // Read a NBS file
-        let bufferReq = new Uint8Array(songFileReq).buffer; // Create an ArrayBuffer
-        let songReq = fromArrayBuffer(bufferReq); // Parse song from ArrayBuffer
-
-        workers.forEach(async worker => {
-          setTimeout(() => { worker.handle(command, username); }, 500);
-        })
+        const songFileReq = fs.readFileSync("songs/" + cmd.setup);
+        const songReq = readNBS(new Uint8Array(songFileReq));
+        workers.forEach((worker) => {
+          setTimeout(() => {
+            worker.handle(command, username);
+          }, 500);
+        });
       }
       break;
     case "tune":
@@ -114,92 +131,77 @@ function handle(command, username) {
       if (!isValidFile(cmd.tune)) {
         respond(`${cmd.tune} is not a valid file!`, 1);
       } else {
-        let songFileTune = fs.readFileSync("songs/" + cmd.tune); // Read a NBS file
-        let bufferTune = new Uint8Array(songFileTune).buffer; // Create an ArrayBuffer
-        let songTune = fromArrayBuffer(bufferTune); // Parse song from ArrayBuffer
-
-        workers.forEach(async worker => {
-          setTimeout(() => { worker.handle(command, username); }, 500);
-        })
+        const songFileTune = fs.readFileSync("songs/" + cmd.tune);
+        const songTune = readNBS(new Uint8Array(songFileTune));
+        workers.forEach((worker) => {
+          setTimeout(() => {
+            worker.handle(command, username);
+          }, 500);
+        });
       }
       break;
     case "stop":
       respond(`Stopping`);
-      stop()
+      stop();
       break;
   }
 }
 
 function isValidFile(name) {
   try {
-    if (fs.existsSync("songs/" + name)) {
-      return true;
-    } else {
-      return false;
-    }
-  } catch(err) {
+    return fs.existsSync("songs/" + name);
+  } catch (err) {
     return false;
   }
 }
 
 function stop() {
-  clearInterval(module.exports.currentSong);
+  clearInterval(currentSong);
 }
 
 function play(songBuffer, speed) {
   stop();
 
   let ready = true;
-  workers.forEach(worker => {
+  workers.forEach((worker) => {
     if (!worker.isTunedAndReady(songBuffer)) ready = false;
-  })
+  });
 
   if (ready) {
-  
-    workers.forEach(async worker => {
+    workers.forEach((worker) => {
       worker.detect();
-    })
+    });
 
-    var tick = 0
-    module.exports.currentSong = setInterval(() => {
+    let tick = 0;
+    currentSong = setInterval(() => {
       runJob(songBuffer, tick);
       tick += 1;
-    }, speed)
-
+    }, speed);
   } else {
-
-    workers.forEach(worker => {
+    workers.forEach((worker) => {
       worker.tune(songBuffer);
       respond("Worker " + worker.options.username + ":", 2);
       worker.prettyRequirements(songBuffer);
-    })
-
+    });
   }
 }
 
 async function runJob(songBuffer, tick) {
-  // Iterate each layer
   for (let currentLayer = 0; currentLayer < songBuffer.layers.length; currentLayer++) {
     const layer = songBuffer.layers[currentLayer];
     const note = layer.notes[tick];
 
     if (note) {
-      let pitch = note.key - 33;
-      workers.forEach(async worker => {
+      const pitch = note.key - 33;
+      workers.forEach((worker) => {
         worker.play_note(note.instrument, pitch);
-      })
+      });
     }
   }
 }
 
 function twoNum(num) {
-  if (num > 9) {
-    return num.toString();
-  } else {
-    return `0${num.toString()}`;
-  }
+  return num > 9 ? num.toString() : `0${num.toString()}`;
 }
 
-module.exports.getBot = () => {
-	return bot;
-}
+export { bot, currentSong };
--- multi.js
+++ multi.js
@@ -1,39 +1,43 @@
-const fs = require("fs");
-const { fromArrayBuffer, SongInstrument } = require("@encode42/nbs.js"); // Import NBS.js
-const chalk = require('chalk');
-var mineflayer = require("mineflayer");
-var parseSentence = require("minimist-string");
-var block_mapper = require("./block_mapper.js")
-var config = require("./config.json");
-var instruments = require('./instruments_map.json');
+import fs from "fs";
+import { read as readNBS } from "@nbsjs/core";
+import chalk from "chalk";
+import mineflayer from "mineflayer";
+import parseSentence from "minimist-string";
+import blockMapper from "./block_mapper.js";
+import { createRequire } from "module";
 
-class NoteBot {
+const require = createRequire(import.meta.url);
+const config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
+const instruments = JSON.parse(fs.readFileSync("./instruments_map.json", "utf-8"));
+
+export class NoteBot {
   constructor(username) {
     this.options = {
       username: username,
       host: config.bot.host || "localhost",
       port: config.bot.port || 25565,
       version: config.bot.version || "1.20.1",
-    }
+    };
     this.bot = mineflayer.createBot(this.options);
     this.availableNoteblocks = {};
+    this.currentSong = null;
 
     this.bot.on("login", () => {
       this.respond(this.options.username + " ONLINE");
     });
 
     this.bot.on("kicked", (reason) => {
-      this.respond(`I got kicked for ${reason}`, 2)
-      this.respond(`Rage Quitting`, 2)
-    })
+      this.respond(`I got kicked for ${reason}`, 2);
+      this.respond(`Rage Quitting`, 2);
+    });
 
     this.bot.on("chat", (username, message) => {
-      if (username == this.options.username) {
-        this.respond(message)
+      if (username === this.options.username) {
+        this.respond(message);
       }
     });
 
-    this.bot.on("whisper", (username, message, rawMessage) => {
+    this.bot.on("whisper", (username, message) => {
       if (config.commands_perms.includes(username)) {
         this.handle(message, username);
       }
@@ -44,46 +48,38 @@ class NoteBot {
     let mes = message;
     switch (level) {
       case -1:
-        mes = chalk.green("[DEBUG] " + message); // debug
+        mes = chalk.green("[DEBUG] " + message);
         break;
       case 0:
-        mes = chalk.blue("[INFO]  " + message); // info
+        mes = chalk.blue("[INFO]  " + message);
         break;
       case 1:
-        mes = chalk.yellow("[WARN]  " + message); // warning
+        mes = chalk.yellow("[WARN]  " + message);
         break;
       case 2:
-        mes = chalk.red("[ERROR] " + message); // error
+        mes = chalk.red("[ERROR] " + message);
         break;
     }
     console.log(mes);
-    // if (level >= 1) {
-    //   beep();
-    // }
-  }
-
-  beep() {
-    require("child_process").exec("powershell.exe [console]::beep(500,600)");
   }
 
   handle(command, username) {
-    var cmd = command.substring(1);
-    cmd = parseSentence(cmd);
-    delete cmd["_"]
+    const cmd = parseSentence(command.substring(1));
+    delete cmd["_"];
 
-    switch(Object.keys(cmd)[0]) {
+    const action = Object.keys(cmd)[0];
+
+    switch (action) {
       case "detect":
-        this.detect()
+        this.detect();
         break;
       case "play":
         this.respond(`Playing ${cmd.play}`);
         if (!this.isValidFile(cmd.play)) {
           this.respond(`${cmd.play} is not a valid file!`, 1);
         } else {
-          let songFile = fs.readFileSync("songs/" + cmd.play); // Read a NBS file
-          let buffer = new Uint8Array(songFile).buffer; // Create an ArrayBuffer
-          let song = fromArrayBuffer(buffer); // Parse song from ArrayBuffer
-
+          const songFile = fs.readFileSync("songs/" + cmd.play);
+          const song = readNBS(new Uint8Array(songFile));
           this.play(song, cmd.speed || 100);
         }
         break;
@@ -91,10 +87,8 @@ class NoteBot {
         if (!this.isValidFile(cmd.setup)) {
           this.respond(`${cmd.setup} is not a valid file!`, 1);
         } else {
-          let songFileReq = fs.readFileSync("songs/" + cmd.setup); // Read a NBS file
-          let bufferReq = new Uint8Array(songFileReq).buffer; // Create an ArrayBuffer
-          let songReq = fromArrayBuffer(bufferReq); // Parse song from ArrayBuffer
-
+          const songFileReq = fs.readFileSync("songs/" + cmd.setup);
+          const songReq = readNBS(new Uint8Array(songFileReq));
           this.prettyRequirements(songReq);
         }
         break;
@@ -103,10 +97,8 @@ class NoteBot {
         if (!this.isValidFile(cmd.tune)) {
           this.respond(`${cmd.tune} is not a valid file!`, 1);
         } else {
-          let songFileTune = fs.readFileSync("songs/" + cmd.tune); // Read a NBS file
-          let bufferTune = new Uint8Array(songFileTune).buffer; // Create an ArrayBuffer
-          let songTune = fromArrayBuffer(bufferTune); // Parse song from ArrayBuffer
-
+          const songFileTune = fs.readFileSync("songs/" + cmd.tune);
+          const songTune = readNBS(new Uint8Array(songFileTune));
           this.tune(songTune);
         }
         break;
@@ -123,70 +115,69 @@ class NoteBot {
 
   isValidFile(name) {
     try {
-      if (fs.existsSync("songs/" + name)) {
-        return true;
-      } else {
-        return false;
-      }
-    } catch(err) {
+      return fs.existsSync("songs/" + name);
+    } catch (err) {
       return false;
     }
   }
 
   tune(songBuffer) {
-    let req = this.findRequirements(songBuffer);
-    let blocks = this.detect_noteblocks();
+    const req = this.findRequirements(songBuffer);
+    const blocks = this.detect_noteblocks();
 
-    Object.keys(req).forEach(instrument_id => {
-      let notes = req[instrument_id];
-      notes.forEach(pitch => {
-        // Find an available block for the instrument_id
-        let availableBlocks = blocks[instrument_id];
+    Object.keys(req).forEach((instrument_id) => {
+      const notes = req[instrument_id];
+      notes.forEach((pitch) => {
+        const availableBlocks = blocks[instrument_id];
         if (availableBlocks) {
-          let blockToTune = availableBlocks.find(block => !block.isTuned);
+          const blockToTune = availableBlocks.find((block) => !block.isTuned);
 
           if (blockToTune) {
-            // Tune the block to the requested pitch
             this.tuneNoteblock(blockToTune, pitch);
-
-            // Mark the block as tuned to avoid re-tuning it
             blockToTune.isTuned = true;
           } else {
-            this.respond(`No available block for instrument ${instrument_id} and pitch ${pitch}`, 1);
+            this.respond(
+              `No available block for instrument ${instrument_id} and pitch ${pitch}`,
+              1
+            );
           }
         } else {
-          this.respond(`No available block for instrument ${instrument_id} and pitch ${pitch}`, 1);
+          this.respond(
+            `No available block for instrument ${instrument_id} and pitch ${pitch}`,
+            1
+          );
         }
       });
     });
   }
 
- tuneNoteblock(block, pitch) {
-    if(block == null) {
+  tuneNoteblock(block, pitch) {
+    if (block === null) {
       return;
     }
-    if(block.pitch == pitch) {
+    if (block.pitch === pitch) {
       return;
     }
 
     let play_times = 0;
-    if (pitch - block.pitch < 0)
-      play_times = 25-(block.pitch-pitch);
-    else
-      play_times = pitch-block.pitch;
-    var timeouts = []
-    for (i = 0; i < play_times; i++) {
-      timeouts[i] = setTimeout(() => {
-        this.bot._client.write('block_place', {
+    if (pitch - block.pitch < 0) {
+      play_times = 25 - (block.pitch - pitch);
+    } else {
+      play_times = pitch - block.pitch;
+    }
+
+    for (let i = 0; i < play_times; i++) {
+      setTimeout(() => {
+        this.bot._client.write("block_place", {
           location: block.position,
           direction: 1,
           hand: 0,
           cursorX: 0.5,
           cursorY: 0.5,
-          cursorZ: 0.5
-        });    
-      }, config.settings.tune_speed*i);
-    } 
+          cursorZ: 0.5,
+        });
+      }, config.settings.tune_speed * i);
+    }
 
     block.pitch = pitch;
   }
@@ -194,10 +185,10 @@ class NoteBot {
   isTunedAndReady(songBuffer) {
     let goodToGo = true;
 
-    let req = this.findNeededRequirements(songBuffer);
-    Object.keys(req).forEach(instrument_id => {
+    const req = this.findNeededRequirements(songBuffer);
+    Object.keys(req).forEach((instrument_id) => {
       if (req[instrument_id].length > 0) goodToGo = false;
-    })
+    });
 
     return goodToGo;
   }
@@ -209,47 +200,45 @@ class NoteBot {
 
     if (this.isTunedAndReady(songBuffer)) {
       this.detect();
-      var tick = 0
+      let tick = 0;
       this.currentSong = setInterval(() => {
         this.runJob(songBuffer, tick);
         tick += 1;
-      }, speed)
+      }, speed);
     } else {
       this.tune(songBuffer);
       setTimeout(() => {
-        if(this.isTunedAndReady(songBuffer)) {
+        if (this.isTunedAndReady(songBuffer)) {
           this.play(songBuffer, speed);
         } else {
           this.prettyRequirements(songBuffer);
         }
-      }, 3000)
+      }, 3000);
     }
   }
 
   async runJob(songBuffer, tick) {
-    // Iterate each layer
     for (let currentLayer = 0; currentLayer < songBuffer.layers.length; currentLayer++) {
       const layer = songBuffer.layers[currentLayer];
       const note = layer.notes[tick];
 
       if (note) {
-        let pitch = note.key - 33;
+        const pitch = note.key - 33;
         this.play_note(note.instrument, pitch);
-        //play_note(0, pitch);
       }
     }
   }
 
   findRequirements(songBuffer) {
-    var needed = {};
+    const needed = {};
 
     for (let currentLayer = 0; currentLayer < songBuffer.layers.length; currentLayer++) {
       const layer = songBuffer.layers[currentLayer];
-      layer.notes.forEach(note => {
+      layer.notes.forEach((note) => {
         if (note) {
-          let pitch = (note.key - 33).toString();
+          const pitch = (note.key - 33).toString();
 
-          if(!(note.instrument in needed)) {
+          if (!(note.instrument in needed)) {
             needed[note.instrument] = [];
           }
 
@@ -257,123 +246,125 @@ class NoteBot {
             needed[note.instrument].push(pitch);
           }
         }
-      })
+      });
     }
 
     return needed;
   }
 
   findNeededRequirements(songBuffer) {
-    var needed = {};
-    var myNoteblocks = {};
+    const needed = {};
+    const myNoteblocks = {};
 
-    let noteblocks = block_mapper.mapnoteblocks(this.bot);
-    noteblocks.forEach(item => {
-      let instrument_id = item.instrumentid;
-      let pitch = item.pitch.toString();
+    const noteblocks = blockMapper.mapnoteblocks(this.bot);
+    noteblocks.forEach((item) => {
+      const instrument_id = item.instrumentid;
+      const pitch = item.pitch.toString();
 
-      if(!(instrument_id in myNoteblocks)) {
+      if (!(instrument_id in myNoteblocks)) {
         myNoteblocks[instrument_id] = [];
       }
 
       myNoteblocks[instrument_id].push(pitch);
-    })
-
+    });
 
     for (let currentLayer = 0; currentLayer < songBuffer.layers.length; currentLayer++) {
       const layer = songBuffer.layers[currentLayer];
-      layer.notes.forEach(note => {
+      layer.notes.forEach((note) => {
         if (note) {
-          let pitch = (note.key - 33).toString();
+          const pitch = (note.key - 33).toString();
 
-          if(!(note.instrument in needed)) {
+          if (!(note.instrument in needed)) {
             needed[note.instrument] = [];
           }
 
-          if(!(note.instrument in myNoteblocks)) {
+          if (!(note.instrument in myNoteblocks)) {
             myNoteblocks[note.instrument] = [];
           }
 
-          if (!needed[note.instrument].includes(pitch) && !myNoteblocks[note.instrument].includes(pitch)) {
+          if (
+            !needed[note.instrument].includes(pitch) &&
+            !myNoteblocks[note.instrument].includes(pitch)
+          ) {
             needed[note.instrument].push(pitch);
           }
         }
-      })
+      });
     }
 
     return needed;
   }
 
   prettyRequirements(songBuffer) {
-    var needed = {};
-    var myNoteblocks = {};
+    const needed = {};
+    const myNoteblocks = {};
 
-    let noteblocks = block_mapper.mapnoteblocks(this.bot);
-    noteblocks.forEach(item => {
-      let instrument_id = item.instrumentid;
-      let pitch = item.pitch.toString();
+    const noteblocks = blockMapper.mapnoteblocks(this.bot);
+    noteblocks.forEach((item) => {
+      const instrument_id = item.instrumentid;
+      const pitch = item.pitch.toString();
 
-      if(!(instrument_id in myNoteblocks)) {
+      if (!(instrument_id in myNoteblocks)) {
         myNoteblocks[instrument_id] = [];
       }
 
       myNoteblocks[instrument_id].push(pitch);
-    })
-
+    });
 
     for (let currentLayer = 0; currentLayer < songBuffer.layers.length; currentLayer++) {
       const layer = songBuffer.layers[currentLayer];
-      layer.notes.forEach(note => {
+      layer.notes.forEach((note) => {
         if (note) {
-          let pitch = (note.key - 33).toString();
+          const pitch = (note.key - 33).toString();
 
-          if(!(note.instrument in needed)) {
+          if (!(note.instrument in needed)) {
             needed[note.instrument] = [];
           }
 
-          if(!(note.instrument in myNoteblocks)) {
+          if (!(note.instrument in myNoteblocks)) {
             myNoteblocks[note.instrument] = [];
           }
 
-          if (!needed[note.instrument].includes(pitch) && !myNoteblocks[note.instrument].includes(pitch)) {
+          if (
+            !needed[note.instrument].includes(pitch) &&
+            !myNoteblocks[note.instrument].includes(pitch)
+          ) {
             needed[note.instrument].push(pitch);
           }
         }
-      })
+      });
     }
 
     let list = "Add the following note blocks:\n";
-    Object.keys(needed).forEach(instrument => {
-      list += `${instruments.blocks[instrument].toUpperCase()} x${this.twoNum(needed[instrument.toString()].length)} \n`;
-    })
+    Object.keys(needed).forEach((instrument) => {
+      list += `${instruments.blocks[instrument].toUpperCase()} x${this.twoNum(
+        needed[instrument.toString()].length
+      )} \n`;
+    });
 
     this.respond(list, 1);
   }
 
   twoNum(num) {
-    if (num > 9) {
-      return num.toString();
-    } else {
-      return `0${num.toString()}`;
-    }
+    return num > 9 ? num.toString() : `0${num.toString()}`;
   }
 
   detect_noteblocks() {
-    let myNoteblocks = {};
-    let noteblocks = block_mapper.mapnoteblocks(this.bot);
+    const myNoteblocks = {};
+    const noteblocks = blockMapper.mapnoteblocks(this.bot);
 
-    noteblocks.forEach(item => {
-      let instrument_id = item.instrumentid;
-      let pitch = item.pitch;
-      let position = item.position;
+    noteblocks.forEach((item) => {
+      const instrument_id = item.instrumentid;
+      const pitch = item.pitch;
+      const position = item.position;
 
-      if(!(instrument_id in myNoteblocks)) {
+      if (!(instrument_id in myNoteblocks)) {
         myNoteblocks[instrument_id] = [];
       }
 
-      let info = { position: position, pitch: pitch };
+      const info = { position: position, pitch: pitch };
       myNoteblocks[instrument_id].push(info);
-    })
+    });
 
     return myNoteblocks;
   }
@@ -383,55 +374,57 @@ class NoteBot {
     this.availableNoteblocks = this.detect_noteblocks();
 
     let numDetected = 0;
-    let values = Object.keys(this.availableNoteblocks).map((key) => { return this.availableNoteblocks[key]; });
-    values.forEach(instrument => {
+    const values = Object.keys(this.availableNoteblocks).map(
+      (key) => this.availableNoteblocks[key]
+    );
+    values.forEach((instrument) => {
       numDetected += instrument.length;
-    })
+    });
 
-    this.respond(`Found ${numDetected}!`)
+    this.respond(`Found ${numDetected}!`);
   }
 
   play_note(instrument_id, pitch) {
-    //detect();
-
-    if(instrument_id in this.availableNoteblocks) {
+    if (instrument_id in this.availableNoteblocks) {
       let target = null;
-      let blocks = this.availableNoteblocks[instrument_id.toString()];
-      blocks.forEach(block => {
-        if (block.pitch.toString() == pitch.toString()) {
+      const blocks = this.availableNoteblocks[instrument_id.toString()];
+      blocks.forEach((block) => {
+        if (block.pitch.toString() === pitch.toString()) {
           target = block;
         }
-      })
-      if (target != null) { 
+      });
+      if (target !== null) {
         this.play_note_by_block(target);
-        //this.respond(`Playing ${pitch} on ${instrument_id}`);
       } else {
-        this.respond(`Pitch ${pitch} not available for instrument ${instrument_id}. (${instruments.blocks[instrument_id.toString()]})`, 1);
+        this.respond(
+          `Pitch ${pitch} not available for instrument ${instrument_id}. (${instruments.blocks[instrument_id.toString()]})`,
+          1
+        );
         clearInterval(this.currentSong);
       }
       return;
     } else {
-      this.respond(`Instrument ${instrument_id} not available. (${instruments.blocks[instrument_id.toString()]})`, 1);
+      this.respond(
+        `Instrument ${instrument_id} not available. (${instruments.blocks[instrument_id.toString()]})`,
+        1
+      );
       return;
     }
   }
 
   play_note_by_block(block) {
-    let position = block.position;
-    this.bot.lookAt(position, true)
-    //let updatedblock = this.bot.blockAt(position, extraInfos=true)
+    const position = block.position;
+    this.bot.lookAt(position, true);
 
-    this.bot._client.write('block_dig', {
+    this.bot._client.write("block_dig", {
       status: 0,
       location: position,
-      face: 1
+      face: 1,
     });
-    this.bot._client.write('block_dig', {
+    this.bot._client.write("block_dig", {
       status: 1,
       location: position,
-      face: 1
+      face: 1,
     });
   }
 }
-
-module.exports.NoteBot = NoteBot;
